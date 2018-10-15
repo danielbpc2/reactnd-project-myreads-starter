8bc85ac6f4a6eeb4bd4a1375160c95af2a6ef906
Daniel Carvalho <danielbpc2@hotmail.com>
2018-10-15 14:43:57 -0700

    Review: BookShelf Review fix

--- src/bookshelf.js
+++ src/bookshelf.js
@@ -2,24 +2,20 @@ import React from 'react';
 import Shelf from './shelf.js'
 import PropTypes from 'prop-types';
 
-class BookShelf extends React.Component {
-  render(){
-    return(
-      <div className="list-books-content">
-        <div>
-          <Shelf name={"Currently Reading"} books={this.props.books.filter(book => book['shelf'] === "currentlyReading")} handleSelection={this.props.handleSelection}/>
-          <Shelf name={"Want to Read"} books={this.props.books.filter(book => book['shelf'] === "wantToRead")} handleSelection={this.props.handleSelection}/>
-          <Shelf name={"Read"} books={this.props.books.filter(book => book['shelf'] === "read")} handleSelection={this.props.handleSelection}/>
-        </div>
-      </div>
-    )
-  }
-}
+const bookShelf = ( { books, handleSelection } ) => (
+  <div className="list-books-content">
+    <div>
+      <Shelf name={"Currently Reading"} books={books.filter(book => book['shelf'] === "currentlyReading")} handleSelection={handleSelection}/>
+      <Shelf name={"Want to Read"} books={books.filter(book => book['shelf'] === "wantToRead")} handleSelection={handleSelection}/>
+      <Shelf name={"Read"} books={books.filter(book => book['shelf'] === "read")} handleSelection={handleSelection}/>
+    </div>
+  </div>
+)
 
-BookShelf.propTypes = {
+bookShelf.propTypes = {
   books: PropTypes.array.isRequired,
   filterBook: PropTypes.func.isRequired,
   handleSelection: PropTypes.func.isRequired
 };
 
-  export default BookShelf;
+  export default bookShelf;
