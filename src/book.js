0414c4a774b266dcd27404a3a537e8ac9cfa0b38
Daniel Carvalho <danielbpc2@hotmail.com>
2018-10-15 14:39:22 -0700

    Review: Fix and added suggestions

--- src/book.js
+++ src/book.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ShelfChanger from './shelfchanger.js';
 import PropTypes from 'prop-types';
 
-const book = (props) => (
+const book = ({bookInfo, handleSelection}) => (
   <div className="book">
     <div className="book-top">
-      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url( ${ props.bookInfo['imageLinks'] == null ? '' : props.bookInfo['imageLinks']['thumbnail'] } )` } }></div>
-      <ShelfChanger book={props.bookInfo} handleSelection={props.handleSelection}/>
+      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url( ${ bookInfo['imageLinks'] == null ? '' : bookInfo['imageLinks']['thumbnail'] } )` } }></div>
+      <ShelfChanger book={bookInfo} handleSelection={handleSelection}/>
     </div>
 
-    <div className="book-title">{props.bookInfo['title']}</div>
+    <div className="book-title">{bookInfo['title']}</div>
 
-    <div className="book-authors">{props.bookInfo['authors'] == null ? "Unknown Authors" : props.bookInfo['authors'].join(', ')}</div>
+    <div className="book-authors">{bookInfo['authors'] == null ? "Unknown Authors" : bookInfo['authors'].join(', ')}</div>
   </div>
   )
 
